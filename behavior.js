ab9e3c529c0005bb6f2b53248f9f901d40f24546
joakimwidell <joakimwidell@hotmail.com>
2021-02-19 14:23:42 +0100

    safety commit, API working!

--- behavior.js
+++ behavior.js
@@ -1,9 +1,26 @@
-let artist = "";
-async function getArtist(artist) {
-    let artistApiUrl = 'https://www.songsterr.com/a/ra/songs.json?pattern=' + artist;
+var artistSearch = "";
+var song = "";
+var artistApiUrl = 'http://www.songsterr.com/a/ra/songs/byartists.json?artists=';
+
+
+function getFormData() {
+    artistSearch = document.getElementById('e-mail-log-in').value;
+    song = document.getElementById('password-log-in').value;
+    document.getElementById("test-p").innerHTML = artistSearch + " " + song;
+    return artistSearch;
+}
+
+async function getArtist() {
+    getFormData();
+    artistApiUrl += artistSearch;
     const response = await fetch(artistApiUrl);
-    const data = await response.json();
-                
-    console.log(data[0].artist.name);
+    const apiData = await response.json();
+    console.log(apiData);
     
 }
+
+/*function parseApiData() {
+    getArtist();
+    var songId = apidata[0][0];
+    console.log(songId);
+}*/
